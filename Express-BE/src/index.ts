72d12371522e7321bf5b45736e583109dbd13502
Zoro <jagbetuyi001@gmail.com>
2024-08-29 14:05:36 +0100

    final deployment test

--- Express-BE/src/index.ts
+++ Express-BE/src/index.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 
 import express from "express";
+import cors from "cors";
 
 import router from "./routes/orderRoute";
 
 const app = express();
 const PORT = process.env.PORT || 2121;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
